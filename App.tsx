123e3551b84f100cadb19b9dc1faff68a4d641fb
Rafael Santos <rafa.05125@gmail.com>
2024-04-09 21:32:04 -0300

    Flex direction

--- App.tsx
+++ App.tsx
@@ -9,9 +9,13 @@ function RedBox() {
 
 function BlueBox() {
   return (
-    <View
-      style={{ height: 100, width: 100, backgroundColor: "blue", flex: 1 }}
-    ></View>
+    <View style={{ height: 100, width: 100, backgroundColor: "blue" }}></View>
+  );
+}
+
+function GreenBox() {
+  return (
+    <View style={{ height: 100, width: 100, backgroundColor: "green" }}></View>
   );
 }
 
@@ -20,7 +24,7 @@ export default function App() {
     <View style={styles.container}>
       <RedBox></RedBox>
       <BlueBox></BlueBox>
-      <RedBox></RedBox>
+      <GreenBox></GreenBox>
       <StatusBar style="auto" />
     </View>
   );
@@ -29,6 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row-reverse",
     borderWidth: 5,
     borderColor: "purple",
     backgroundColor: "#fff",
